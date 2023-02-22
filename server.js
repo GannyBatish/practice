252a2bff40f9c5aef9e1001bbbd4214ad3412665
Ganny Batish <ganny0223.be20@chitkara.edu.in>
2023-02-22 09:52:16 +0530

    Server created

---
+++ server.js
@@ -0,0 +1,14 @@
+const express=require('express');  //framework for nodejs
+
+//create server
+const app=express();
+//routes
+app.get('/',(req,res)=>{
+    res.send('Server is Running')
+})
+app.get('/api',(req,res)=>{
+    res.send('API');
+})
+
+//kise post te on krna server nu
+app.listen(5001,console.log('Listening at PORT http://localhost:5000'));
